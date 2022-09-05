c354b07a7fd176ef429b2c72787e2c5c7375fa6d
g.struchin <g.struchin@gmail.com>
2022-09-06 03:23:05 +0400

    task and task with an asterisk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,23 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.CharBuffer;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        char c = '9';
-        System.out.println((int)c);
+        //long number = (long) (Math.random() * 9223372036854L);
 
-        String url = "http://numbersapi.com/100500/trivia";
+        BigInteger number = BigInteger.probablePrime(72, new Random());
+
+        String url = "http://numbersapi.com/"+number+"/trivia";
 
         URL obj = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
@@ -33,8 +35,8 @@ public class Main {
         }
         in.close();
 
-        System.out.println(response.toString());
-        System.out.println();
+        /*System.out.println(response.toString());
+        System.out.println();*/
 
 
         //for(char c : response.)
@@ -61,12 +63,68 @@ public class Main {
             }
         }
 
+        System.out.println("Частоты:");
         for (Map.Entry entry: frequency.entrySet()) {
 
             System.out.println(entry);
 
         }
-        System.out.println(sumChar);
-        System.out.println(frequency.size());
+
+
+        float averageFrequency = (float)sumChar/frequency.size();
+        int divFrequency = sumChar/frequency.size();
+
+        ArrayList<Character> nearChars = new ArrayList<Character>();
+        int nearCountChar = 0;
+
+        /*System.out.println(sumChar);
+        System.out.println(frequency.size());*/
+        System.out.println("Среднее значение частоты "+sumChar+"/"+frequency.size()+"="+averageFrequency);
+
+        for (Map.Entry entry: frequency.entrySet()) {
+            if(nearCountChar == 0) {
+                nearCountChar = (int)entry.getValue();
+            }
+            if ((int)entry.getValue() == nearCountChar) {
+                nearChars.add((Character) entry.getKey());
+                continue;
+            } else
+            if (sumChar%frequency.size()*2==frequency.size()){
+                if (nearCountChar > (int)divFrequency){
+                    if ((int)entry.getValue() == divFrequency-(nearCountChar-divFrequency-1)) {
+                        nearChars.add((Character) entry.getKey());
+                        nearCountChar = (int) entry.getValue();
+                        continue;
+                    }
+                } else{
+                    if ((int)entry.getValue() == divFrequency+divFrequency-nearCountChar+1) {
+                        nearChars.add((Character) entry.getKey());
+                        nearCountChar = (int) entry.getValue();
+                        continue;
+                    }
+                }
+            }
+            Integer val = (Integer) entry.getValue();
+            float d1 = (float)val - averageFrequency;
+            d1 = Math.abs(d1);
+            float d2 = (float)nearCountChar - averageFrequency;
+            d2 = Math.abs(d2);
+            if (d1 < d2) {
+                nearChars.clear();
+                nearCountChar = (int) entry.getValue();
+                nearChars.add((Character) entry.getKey());
+            }
+        }
+
+        StringJoiner joiner = new StringJoiner(", ");
+
+        for (Character ch: nearChars) {
+
+            joiner.add(ch.toString()+"("+(int)ch+")");
+        }
+
+        System.out.println("Символы, которые соответствуют условию наиболее близкого значения частоты к среднему значанию: "+joiner);
+
+        //nearChars.forEach(System.out::println);
     }
 }
